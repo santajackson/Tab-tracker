f4fb1426bf5585f3f392f3856c7ce353ed6c5105
santajackson <darryljackson@Darryls-MacBook-Pro.local>
2018-03-30 02:34:16 -0400

    setup of the express js server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,18 @@
 console.log('hello')
+const express = require('express')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyparser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'Whats poppin cuhh'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
